0591c9eed70e7f23c2a10fb7031671995999000b
Bruno Milano <brn.milano@hotmail.com>
2023-10-22 21:12:52 -0300

    Resolução de bugs

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,6 @@ const listaDeCursos = document.getElementById("lista-de-cursos");
 if (listaDeCursos) {
   listaDeCursos.addEventListener("change", function () {
     const valorSelecionado = this.value;
-
     console.log("Opção selecionada:", valorSelecionado);
 
     const rows = document.querySelectorAll(".cursos tbody tr");
@@ -15,26 +14,23 @@ if (listaDeCursos) {
       if (valorSelecionado === "todos-os-cursos") {
         row.style.display = "";
       }
-
       else if (row.getAttribute("data-course-type") === valorSelecionado) {
         row.style.display = "";
       } else {
         row.style.display = "none";
-      };
+      }
     });
   });
-};
+}
 
 // Funcionalidade do dropdown que fecha e depois abre o dropdown
 function openDropdown(dropdownId) {
   const allDropdowns = document.getElementsByClassName("conteudo-dropdown");
-
   for (let dropdown of allDropdowns) {
     if (dropdown.id !== dropdownId) {
       dropdown.classList.remove("show");
-    };
-  };
-
+    }
+  }
   document.getElementById(dropdownId).classList.toggle("show");
 }
 
@@ -43,26 +39,26 @@ function openDropdown(dropdownId) {
 window.onclick = function (event) {
   if (!event.target.matches(".btn-do-dropdown") && !event.target.matches(".fas")) {
     const dropdowns = document.getElementsByClassName("conteudo-dropdown");
-
     for (let openDropdown of dropdowns) {
       if (openDropdown.classList.contains("show")) {
         openDropdown.classList.remove("show");
       }
-    };
-  };
-};
+    }
+  }
+
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
 
 // Captura os valores de cada link dentro do dropdown
 document.addEventListener("DOMContentLoaded", function () {
   const dropdowns = document.querySelectorAll(".conteudo-dropdown");
-
   dropdowns.forEach(function (dropdown) {
     const links = dropdown.querySelectorAll("a");
-
     links.forEach(function (link) {
       link.addEventListener("click", function (event) {
         event.preventDefault();
-
         console.log(link.textContent);
       });
     });
@@ -84,20 +80,20 @@ document.querySelectorAll('.conteudo-dropdown a[href="#excluir"]').forEach(item 
   });
 });
 
-modalClose.onclick = function () {
-  modal.style.display = "none";
+if (modalClose) {
+  modalClose.onclick = function () {
+    modal.style.display = "none";
+  }
 }
 
-cancelarExclusao.onclick = function () {
-  modal.style.display = "none";
+if (cancelarExclusao) {
+  cancelarExclusao.onclick = function () {
+    modal.style.display = "none";
+  }
 }
 
-confirmarExclusao.onclick = function () {
-  modal.style.display = "none";
-}
-
-window.onclick = function (event) {
-  if (event.target === modal) {
+if (confirmarExclusao) {
+  confirmarExclusao.onclick = function () {
     modal.style.display = "none";
   }
 }
